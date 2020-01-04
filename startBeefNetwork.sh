f3a375223d8ce7234aa7a1b3aaa188ac190513fc
Anderson Felippe <adfelippe@gmail.com>
2020-01-04 14:49:40 -0300

    Update start network script to the new deploy version file

--- startBeefNetwork.sh
+++ startBeefNetwork.sh
@@ -3,6 +3,6 @@
 # Install archive file card
 composer network install --card PeerAdmin@hlfv1 --archiveFile beef-tracer.bna
 # Start beef-tracer network
-composer network start --networkName beef-tracer --networkVersion 0.0.2-deploy.3 --networkAdmin admin --networkAdminEnrollSecret adminpw --card PeerAdmin@hlfv1 --file admin.card
+composer network start --networkName beef-tracer --networkVersion 0.0.2-deploy.24 --networkAdmin admin --networkAdminEnrollSecret adminpw --card PeerAdmin@hlfv1 --file admin.card
 # Start composer playground
 composer-playground

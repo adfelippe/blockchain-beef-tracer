52be727b6e2b1c41efe1b003a584fb023b9d4b78
Anderson Felippe <adfelippe@gmail.com>
2020-07-08 02:21:13 +0000

    update functions are fucked up now

--- beef_contract.js
+++ beef_contract.js
@@ -56,117 +56,117 @@ async function createAnimal(creationRequest) {
 }
 
 /**
- * Update an existing animal
+ * Create a new animal as an asset
  * @param {org.acme.beef_network.updateAnimal} updateAnimal
  * @brief A transaction to update information of a given animal (based on geneticId)
  * @transaction
  */
 async function updateAnimal(updateAnimalRequest) {
 
-  console.log('updateAnimal');
-  const factory = getFactory();
-  const namespace = 'org.acme.beef_network';
-  let transportRegistry = '';
-  let transportCheck = '';
-  let slaughterhouseRegistry = '';
-  let slaughterhouseCheck = '';
+    console.log('updateAnimal');
+    const factory = getFactory();
+    const namespace = 'org.acme.beef_network';
 
-  // Get animal registry and perform proper checks
-  const animalRegistry = await getAssetRegistry(namespace + '.Animal');
-  const idCheck = await animalRegistry.exists(updateAnimalRequest.geneticId.getIdentifier());
-  const animal = await animalRegistry.get(updateAnimalRequest.geneticId.getIdentifier());
+    // Get Animal registry from ledger (if any)
+    const animalRegistry = await getAssetRegistry(namespace + '.Animal');
+    const idCheck = await animalRegistry.exists(updateAnimalRequest.geneticId.getIdentifier());
 
-  // Get Farmer registry and perform proper checks
-  const participantRegistry = await getParticipantRegistry(namespace + '.Farmer');
-  const ownerCheck = await participantRegistry.exists(updateAnimalRequest.owner.getIdentifier());
-  animal.owner = await participantRegistry.get(updateAnimalRequest.owner.getIdentifier());
+    // Get Farmer registry from ledger (if any)
+    const participantRegistry = await getParticipantRegistry(namespace + '.Farmer');
+    const ownerCheck = await participantRegistry.exists(updateAnimalRequest.owner.getIdentifier());
 
-  // Get Transportation registry and perform proper checks
-  if (updateAnimalRequest.transportedBy) {
-    transportRegistry = await getParticipantRegistry(namespace + '.Transportation');
-    transportCheck = await transportRegistry.exists(updateAnimalRequest.transportedBy.getIdentifier());
+    // Get Transportation Company registry from ledger (if any)
+    /*const transportRegistry = await getParticipantRegistry(namespace + '.Transportation');
+    const transportCheck = await transportRegistry.exists(updateAnimalRequest.transportedBy.getIdentifier());
+
+    const slaughterhouseRegistry = await getParticipantRegistry(namespace + '.Slaughterhouse');
+    const slaughterhouseCheck = await slaughterhouseRegistry.exists(updateAnimalRequest.slaughterhouse.getIdentifier());*/
+
     // Throw error and leave if transportation is to be updated but company doesn't exist
-    if (!transportCheck) {
-        throw new Error("This Transportation company does not exist!")
+    if (!idCheck) {
+        throw new Error("This Animal does not exist!")
         return;
     }
-  }
 
-  // Get Slaughterhouse registry and perform proper checks
-  if (updateAnimalRequest.slaughterhouse) {
-    slaughterhouseRegistry = await getParticipantRegistry(namespace + '.Slaughterhouse');
-    slaughterhouseCheck = await slaughterhouseRegistry.exists(updateAnimalRequest.slaughterhouse.getIdentifier());
+    // Throw error and leave if transportation is to be updated but company doesn't exist
+    if (!ownerCheck) {
+        throw new Error("This Farmer does not exist!")
+        return;
+    }
+
+    // Throw error and leave if transportation is to be updated but company doesn't exist
+    /*if (!transportCheck) {
+        throw new Error("This Transportation Company does not exist!")
+        return;
+    }
+
     // Throw error and leave if transportation is to be updated but company doesn't exist
     if (!slaughterhouseCheck) {
-        throw new Error("This Slaughterhouse company does not exist!")
+        throw new Error("This Slaughterhouse does not exist!")
         return;
-    }
-  }
+    }*/
 
+    // Get current Animal registry from the ledger
+    let animal = await animalRegistry.get(updateAnimalRequest.geneticId.getIdentifier());
 
-  if (!idCheck) {
-    throw new Error("This Animal's genetic ID does not exist!")
-  } else {
-    // Check existence of owner
-    if (!ownerCheck) {
-    throw new Error('This Farmer does not exist!')
-    } else {
-      // Update animal in the ledger
-      // Optional and mandatory fields are checked againt themselves to be updated
+    // Get Farmer ID from ledger
+    //animal.owner = await participantRegistry.get(updateAnimalRequest.owner.getIdentifier());
 
-      // Mandatory
-      animal.lifeStage = updateAnimalRequest.lifeStage;
+    // Update animal in the ledger
+    // Optional and mandatory fields are checked againt themselves to be updated
+    animal.lifeStage = updateAnimalRequest.lifeStage;
 
-      // Mandatory at creation only
-      if (!animal.weight || (animal.weight && !updateAnimalRequest.weight)) {
+    // Mandatory at creation only
+    if (!animal.weight || (animal.weight && !updateAnimalRequest.weight)) {
         animal.weight = animal.weight;
-      } else if (animal.weight && updateAnimalRequest.weight) {
+    } else if (animal.weight && updateAnimalRequest.weight) {
         animal.weight = animal.weight + ' | ' + updateAnimalRequest.weight;
-      }
-      // Mandatory at creation only
-      if (!animal.location || (animal.location && !updateAnimalRequest.location)) {
-        animal.location = animal.location;
-      } else if (animal.location && updateAnimalRequest.location){
-        animal.location = animal.location + ' | ' + updateAnimalRequest.location;
-      }
-      // Optional
-      if (!animal.animalId) {
-        animal.animalId = updateAnimalRequest.animalId;
-      } else if (animal.animalId && updateAnimalRequest.animalId) {
-        animal.animalId = animal.animalId + ' | ' + updateAnimalRequest.animalId;
-      }
-      // Optional
-      if (!animal.vaccines) {
-        animal.vaccines = updateAnimalRequest.vaccines;
-      } else if (animal.vaccines && updateAnimalRequest.vaccines) {
-        animal.vaccines = animal.vaccines + ' | ' + updateAnimalRequest.vaccines;
-      }
-      // Optional
-      if (!animal.diseases) {
-        animal.diseases = updateAnimalRequest.diseases;
-      } else if (animal.diseases && updateAnimalRequest.diseases) {
-        animal.diseases = animal.diseases + ' | ' + updateAnimalRequest.diseases;
-      }
-      // Optional
-      // Always updated if requested
-      // It is useful if some data needs to be fixed
-      if (updateAnimalRequest.slaughterDate) {
-          animal.slaughterDate = updateAnimalRequest.slaughterDate;
-      }
-      // Optional Transportation data
-      if (updateAnimalRequest.transportedBy) {
-          animal.transportedBy = await transportRegistry.get(updateAnimalRequest.transportedBy.getIdentifier());
-      }
-      // Optional Slaughterhouse data
-      if (updateAnimalRequest.slaughterhouse) {
-          animal.slaughterhouse = await slaughterhouseRegistry.get(updateAnimalRequest.slaughterhouse.getIdentifier());
-      }
-      // Update ledger
-      await animalRegistry.update(animal);
     }
-    // Emit the event
-    //const updateAnimalEvent = factory.newEvent(namespace, 'updateAnimalEvent');
-  }
+
+    // Mandatory at creation only
+    if (!animal.location || (animal.location && !updateAnimalRequest.location)) {
+        animal.location = animal.location;
+    } else if (animal.location && updateAnimalRequest.location) {
+        animal.location = animal.location + ' | ' + updateAnimalRequest.location;
+    }
+
+    // Optional
+    if (!animal.animalId) {
+        animal.animalId = updateAnimalRequest.animalId;
+    } else if (animal.animalId && updateAnimalRequest.animalId) {
+        animal.animalId = animal.animalId + ' | ' + updateAnimalRequest.animalId;
+    }
+
+    // Optional
+    if (!animal.vaccines) {
+        animal.vaccines = updateAnimalRequest.vaccines;
+    } else if (animal.vaccines && updateAnimalRequest.vaccines) {
+        animal.vaccines = animal.vaccines + ' | ' + updateAnimalRequest.vaccines;
+    }
+
+    // Optional
+    if (!animal.diseases) {
+        animal.diseases = updateAnimalRequest.diseases;
+    } else if (animal.diseases && updateAnimalRequest.diseases) {
+        animal.diseases = animal.diseases + ' | ' + updateAnimalRequest.diseases;
+    }
+
+    // Optional
+    // Always updated if requested
+    // It is useful if some data needs to be fixed
+    if (updateAnimalRequest.slaughterDate)
+        animal.slaughterDate = updateAnimalRequest.slaughterDate;
+
+    // Optional Transportation data
+    /*if (updateAnimalRequest.transportedBy)
+        animal.transportedBy = await transportRegistry.get(updateAnimalRequest.transportedBy.getIdentifier());
+
+    // Optional Slaughterhouse data
+    if (updateAnimalRequest.slaughterhouse)
+        animal.slaughterhouse = await slaughterhouseRegistry.get(updateAnimalRequest.slaughterhouse.getIdentifier());*/
+
+    // Update ledger
+    await animalRegistry.update(animal);
 }
 
 /**
@@ -194,7 +194,7 @@ async function createProduct(creationRequest) {
   	creation.productIssues = creationRequest.productIssues;
     creation.geneticId = factory.newRelationship(namespace, 'Animal', creationRequest.geneticId.getIdentifier());
   	creation.slaughterhouse = factory.newRelationship(namespace, 'Slaughterhouse', creationRequest.slaughterhouse.getIdentifier());
-  	
+
   	// Optional
   	if (creationRequest.transportedBy)
       creation.transportedBy = factory.newRelationship(namespace, 'Transportation', creationRequest.transportedBy.getIdentifier());
@@ -207,40 +207,44 @@ async function createProduct(creationRequest) {
     // (Products can only be created if their source animal exists in the ledger)
     const animalRegistry = await getAssetRegistry(namespace + '.Animal');
     const animalCheck = await animalRegistry.exists(creationRequest.geneticId.getIdentifier());
+
     const slaughterhouseRegistry = await getParticipantRegistry(namespace + '.Slaughterhouse');
     const slaughterhouseCheck = await slaughterhouseRegistry.exists(creationRequest.slaughterhouse.getIdentifier());
+
   	const procIndustryRegistry = await getParticipantRegistry(namespace + '.ProcessingIndustry');
     const procIndustryCheck = await procIndustryRegistry.exists(creationRequest.processingCompany.getIdentifier());
+
   	const retailRegistry = await getParticipantRegistry(namespace + '.Retail');
     const retailCheck = await retailRegistry.exists(creationRequest.retailSeller.getIdentifier());
+
   	const transportRegistry = await getParticipantRegistry(namespace + '.Transportation');
     const transportCheck = await transportRegistry.exists(creationRequest.transportedBy.getIdentifier());
-  
+
     if (!animalCheck) {
       throw new Error('This Animal does not exist!')
       return
     }
-    
+
   	if (!slaughterhouseCheck) {
       throw new Error('This Slaughterhouse Company does not exist!')
       return
     }
-  
+
   	if (!procIndustryCheck) {
       throw new Error('This Processing Industry does not exist!')
       return
     }
-  
+
 	if (!retailCheck) {
       throw new Error('This Retail Seller does not exist!')
       return
     }
-  
+
   	if (!transportCheck) {
       throw new Error('This Transportation Company does not exist!')
       return
     }
-  
+
 	// save the order
     const assetRegistry = await getAssetRegistry(creation.getFullyQualifiedType());
     await assetRegistry.add(creation);
@@ -251,38 +255,51 @@ async function createProduct(creationRequest) {
  * @param {org.acme.beef_network.updateAnimal} updateAnimal
  * @brief A transaction to update information of a given animal (based on geneticId)
  * @transaction
- 
+ */
+
 async function updateProduct(updateProductRequest) {
-  	
+
   	console.log('updateProduct');
     const factory = getFactory();
     const namespace = 'org.acme.beef_network';
-  
+
     // Get product registry and perform proper checks
   	const productRegistry = await getAssetRegistry(namespace + '.Product');
   	const idCheck = await productRegistry.exists(updateProductRequest.productId.getIdentifier());
-  	const product = await productRegistry.get(updateProductRequest.productId.getIdentifier());
-  
-  	// Get retail seller registry and perform proper checks
+
+   	// Get retail seller registry and perform proper checks
   	const retailSellerRegistry = await getParticipantRegistry(namespace + '.Retail');
-  	const retailCheck = await participantRegistry.exists(updateAnimalRequest.owner.getIdentifier());
-  	product.retailSeller = await participantRegistry.get(updateAnimalRequest.owner.getIdentifier());
-  
+  	const retailCheck = await retailSellerRegistry.exists(updateProductRequest.retailSeller.getIdentifier());
+
+  	// Get transportation registry and perform proper checks
+  	const transportRegistry = await getParticipantRegistry(namespace + '.Transportation');
+    const transportCheck = await transportRegistry.exists(updateProductRequest.transportedBy.getIdentifier());
+
   	if (!idCheck) {
     	throw new Error("This Product's ID does not exist!")
       	return;
   	}
-  
-    // Get Transportation registry and perform proper checks
- 	if (updateProductRequest.transportedBy) {
-    	transportRegistry = await getParticipantRegistry(namespace + '.Transportation');
-   		transportCheck = await transportRegistry.exists(updateProductRequest.transportedBy.getIdentifier());
-      	// Throw error and leave if transportation is to be updated but company doesn't exist
-    	if (!transportCheck) {
-        throw new Error("This Transportation company does not exist!")
-        return;
-    	}
-  	}
 
-}*/
+  	if (!transportCheck) {
+      throw new Error('This Transportation Company does not exist!')
+      return
+    }
+
+  	if (!retailCheck) {
+      throw new Error('This Retail Company does not exist!')
+      return
+    }
+
+  	// After checks are validm, get productId from the ledger
+  	let product = await productRegistry.get(updateProductRequest.productId.getIdentifier());
+
+  	if (updateProductRequest.retailSeller)
+  		product.retailSeller = await retailSellerRegistry.get(updateProductRequest.retailSeller.getIdentifier());
+
+  	if (updateProductRequest.transportedBy)
+          product.transportedBy = await transportRegistry.get(updateProductRequest.transportedBy.getIdentifier());
+
+  	// Update ledger
+    await productRegistry.update(product);
+}
 
